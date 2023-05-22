8d433543f61a5a25ff092fe0cd4a3fea99c9189d
BlinXSC <alanmark.alegre@gmail.com>
2023-05-22 17:22:29 +0000

    A bit on error handling, Greeting challenge.

---
+++ challenges/greeting.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def main():
+    name = input("What is your name? ")
+    day_of_week = input("What is the day of the week? ")
+
+    print("Hello, " + name + "! Happy " + day_of_week + "!")
+
+if __name__ == "__main__":
+    main()
